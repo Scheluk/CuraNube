0d4cb13f0b1121a4f8bd84901aad7b1892055ba2
MathiasPal <91483835+MathiasPal@users.noreply.github.com>
2022-06-22 00:25:52 +0200

    REST approved

--- Curanube/static/js/code.js
+++ Curanube/static/js/code.js
@@ -25,6 +25,18 @@ function loadImpressum() {
     xhttp.send();
   }
 
+function getTime() {
+    const url = new URL("http://worldclockapi.com/api/json/utc/now")
+   
+    fetch(url, {method: "GET"})
+        .then(response => response.json())
+        .then((data) => {
+            const msgBoard = document.getElementById("display_time")
+            const text = document.createTextNode(JSON.stringify(data))
+            msgBoard.append(text)
+        })
+}
+
 /*async function submitLogin(username, password) {
     const response = await fetch.
     const formData = new FormData(e.target)
