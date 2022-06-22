4ed31e045816586316a2d24f606088a02ea1302b
Dr. Schluk <lukas.schelepet@aon.at>
2022-06-22 18:01:05 +0200

    final commit with comments

--- Curanube/auth/routes.py
+++ Curanube/auth/routes.py
@@ -16,11 +16,11 @@ from sqlalchemy.sql.expression import func
 ### --- login routes --- ###
 
 
-#LOGIN
+#LOGIN route
 @bp.route("/login", methods=["POST", "GET"])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for("root.index"))
+    if current_user.is_authenticated:       #wenn der user bereits eingeloggt ist (invalid route)
+        return redirect(url_for("root.index"))  #geh zur homepage
     if request.method == "POST":    #if POST, do the login
         error = None  #error message that shows up on incorrect login
         #convert the form data to json response, and then to json data
@@ -28,68 +28,71 @@ def login():
             credentials=request.form["username"],
             pw=request.form["password"]
         ).get_json()
-        if "@" in userjson["credentials"]:
-            user = User.query.filter_by(email=userjson["credentials"]).first()
+        if "@" in userjson["credentials"]:  #if user logged in with email
+            user = User.query.filter_by(email=userjson["credentials"]).first()  #get user with that email
         else:
-            user = User.query.filter_by(username=userjson["credentials"]).first()
+            user = User.query.filter_by(username=userjson["credentials"]).first()   #else get the user with that username
         
-        if user is None:
+        if user is None:    #if the user doesnt exist
             error = "Incorrect Username/Email"
-        elif not check_password_hash(user.password, userjson["pw"]):
+        elif not check_password_hash(user.password, userjson["pw"]): #if the password is incorrect
             error = "Incorrect password"
-        elif not user.confirmed:
+        elif not user.confirmed:    #if the user is not comfirmed
             error = "User not verified"
-        if error == None:
-            login_user(user)
-            return redirect(url_for("profile.home", username = user.username))
+        if error == None:   #if none of the above errors apply
+            login_user(user)    #login the user (session handling)
+            return redirect(url_for("profile.home", username = user.username))  #go to the users homepage
         
-        flash(error)
-    error = None
+        flash(error)    #flash an error if one exists
+    error = None    #clear the error
     return render_template("auth/login.html")    #show the login form
 
 
 
 #LOGOUT
 @bp.route("/logout")
-@login_required
+@login_required #requires user to be logged in (no anonymous calling of that route)
 def logout():
-    logout_user()
-    return redirect(url_for("root.index"))
+    logout_user()   #logout user (session handling)
+    return redirect(url_for("root.index"))  #go to index
 
 
 
 ### --- User Creation Routes --- ###
 @bp.route("/accountcreated")
-def accountcreated():
+def accountcreated():   #route when user was successfully created
     return render_template("auth/accountcreated.html")
 
 
 @bp.route("/createaccount", methods=["POST", "GET"])
 def createaccount():
-    error = None  #error message that shows up on incorrect login
-    if request.method == "POST":    #if POST, do the login
+    error = None  #error message that shows up if one exists
+    if request.method == "POST":    #if POST, do the account creation
         #convert the form data to json response, and then to json data
         userjson = jsonify(
             email=request.form["email"],
             username=request.form["username"],
             pw=request.form["password"]
         ).get_json()
-        error = valid_new_account(userjson)
-        if error == None:
+        error = valid_new_account(userjson) #validate if the data for the new user is valid (keine doppelgänger bezüglich username und email)
+        if error == None:   
+            #create a new user object
             user = User(id=freeUserId(), email=userjson["email"], username=userjson["username"], password=generate_password_hash(userjson["pw"]), confirmed=False)
             user.confirmed = True #ONLY BECAUSE GOOGLE WONT LET US SEND VERIFICATION EMAILS!!!
-            db.session.add(user)
-            db.session.commit()
-            token = generate_verification_token(userjson["email"])
-            verify_url = url_for("auth.verify_email", token = token, _external=True)
-            html = render_template("profile/activate.html", verify_url = verify_url)
-            subject = "Please confirm your email"
+            db.session.add(user)    #add the created user to the database
+            db.session.commit() #commit changes to the database, to take effect
+            token = generate_verification_token(userjson["email"])  #generate a token from the newly created users email address
+            
+            #create the url that should be sent to the users registered email, so he can activate his account
+            verify_url = url_for("auth.verify_email", token = token, _external=True)    
+            html = render_template("profile/activate.html", verify_url = verify_url)    #content of the email
+            subject = "Please confirm your email"   #subject of the email
             #NOT LONGER AVAILABLE AS OF 30th OF MAY, 2022
             #send_email(userjson["email"], subject, html)
 
             #login_user(user)
 
-            return redirect(url_for("auth.accountcreated"))
+            return redirect(url_for("auth.accountcreated")) 
         flash(error)
     return render_template("auth/createaccount.html", error = error)
 
