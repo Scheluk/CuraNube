ad02d8c59971c84ec1d2c22701903df31f2ae9a4
Curt96 <92109778+Curt96@users.noreply.github.com>
2022-08-24 13:08:27 +0200

    maximal responsivness

--- Curanube/static/js/code.js
+++ Curanube/static/js/code.js
@@ -31,6 +31,16 @@ function getFact() {
             const breakl = document.createElement('br')
             msgBoard.append(breakl)
         })
+
 }
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("meineBar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
 
 
